76cb4f359d1584c4ab69e914492fc5239bae6fd3
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2022-02-08 21:07:12 +0200

    added documentation

--- clearing_ds.py
+++ clearing_ds.py
@@ -1,24 +1,34 @@
-import pandas as pd
+"""
+Clear IMDB dataset and get geographic coordinates of places from the dataframe
+"""
 import re
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
+import pandas as pd  # pylint: disable=import-error
+from geopy.geocoders import Nominatim  # pylint: disable=import-error
+from geopy.extra.rate_limiter import RateLimiter  # pylint: disable=import-error
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
-df = pd.read_csv("datasets/locations.list", sep="\"|\t+", error_bad_lines=False, encoding_errors="ignore",
-                 skiprows=14, engine='python', names=["shit", "Title", "year", "location"], nrows=500000)
+df = pd.read_csv("datasets/locations.list", sep="\"|\t+",
+                 error_bad_lines=False, encoding_errors="ignore",
+                 skiprows=14, engine='python',
+                 names=["shit", "Title", "year", "location"], nrows=500000)
 del df["shit"]
 
 
 def year_format(bad_year):
+    """
+    remove parentheses and additional info from 'year' column
+    :param bad_year:
+    :return:
+    """
     year = re.sub('[^0-9]', '', bad_year)
     year = re.sub('{[^>]+}', '', year)
     year = re.sub('[()]', '', year)
     return int(year) if len(year) == 4 else 2004
 
 
-df['year'] = df['year'].map(lambda x: year_format(x))
+df['year'] = df['year'].map(year_format)
 new_df = df.head(20000)
 geocoder = RateLimiter(Nominatim(user_agent="sth").geocode, min_delay_seconds=1)
 new_df['location'] = new_df['location'].apply(geocoder)
